28ea946a7a7bd36cab999aee768587f0d051b916
MarcioGCosta <marcio.gustavo.2006@gmail.com>
2023-12-07 20:18:42 -0300

    new

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo sed '/Color/s/^#//' -i /etc/pacman.conf && sudo sed '/ParallelDownloads/s/^
 yes | sudo pacman -Sy reflector && sudo reflector --sort score --save /etc/pacman.d/mirrorlist. && sudo rm /etc/pacman.d/mirrorlist && sudo mv /etc/pacman.d/mirrorlist. /etc/pacman.d/mirrorlist
 
 ## Installing misc progams
-yes | sudo pacman -Syu zsh-syntax-highlighting libvdpau-va-gl xf86-video-intel pulsemixer qbittorrent bat ripgrep htop ufw ntp wireplumber pipewire-jack firefox foot mpv yt-dlp pipewire papirus-icon-theme pipewire-pulse pipewire-alsa libva-intel-driver playerctl dash go zsh noto-fonts-emoji ttf-nerd-fonts-symbols ttf-ibm-plex feh neovim doas yarn npm xsel
+yes | sudo pacman -Syu zsh-syntax-highlighting zsh-autosuggestions libvdpau-va-gl xf86-video-amdgpu vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau mesa pulsemixer qbittorrent bat ripgrep htop ufw ntp wireplumber pipewire-jack foot mpv yt-dlp pipewire papirus-icon-theme pipewire-pulse pipewire-alsa libva-intel-driver playerctl dash go zsh noto-fonts-emoji ttf-nerd-fonts-symbols ttf-ibm-plex feh neovim doas yarn npm xsel sway 
 
 ## Moving folders
 mv $HOME/Arice/config/.zshrc /$HOME/.zshrc && mv $HOME/Arice/config/.config /$HOME/
@@ -23,20 +23,14 @@ sudo ln -sfT dash /usr/bin/sh
 ## Installing yay
 git clone https://aur.archlinux.org/yay.git && cd yay* && yes | makepkg -si && cd .. && rm -rf yay
 
-echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
-
-## zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions && sudo mv zsh-autosuggestions/ /usr/share/zsh/plugins/
-
-
 ## Changing user shell to zsh
 sudo chsh -s /usr/bin/zsh marcioc
 
 ## Configuring doas and Configuring neovim for doas/su 
-sudo mv $HOME/Arice/config/doas.conf /etc/ && sudo cp -r $HOME/.config/nvim/ /root/.config/
+touch doas.conf && echo permit nopass marcioc as root > doas.conf && sudo mv doas.conf /etc/ && sudo chown root:root /etc/doas.conf
 
 ## Installing misc programs in yay 
-yay -S pfetch orphan-manager dashbinsh ly 
+yay -S pfetch orphan-manager dashbinsh ly mercury-browser-bin
 
 ## Enabling daemons
 sudo systemctl enable ufw.service && sudo systemctl enable ntpd.service && sudo systemctl enable ly.service
