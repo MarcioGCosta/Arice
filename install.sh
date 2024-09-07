206f12e5758199c4564f1f0588e23044cb8f2ede
MarcioGCosta <marcio.gustavo.2006@gmail.com>
2024-09-07 10:51:37 -0300

    oaeu-

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo sed '/Color/s/^#//' -i /etc/pacman.conf && sudo sed '/ParallelDownloads/s/^
 yes | sudo pacman -Sy reflector && sudo reflector --sort score --save /etc/pacman.d/mirrorlist. && sudo rm /etc/pacman.d/mirrorlist && sudo mv /etc/pacman.d/mirrorlist. /etc/pacman.d/mirrorlist
 
 ## Installing misc progams
-sudo pacman -Syu swaybg zsh-syntax-highlighting ttf-hack rofi zsh-autosuggestions libvdpau-va-gl xf86-video-amdgpu vulkan-radeon xorg-xwayland libva-mesa-driver qemu virt-manager edk2-ovmf dnsmasq mesa-vdpau mesa pulsemixer qbittorrent bat ripgrep htop ufw ntp wireplumber pipewire-jack foot mpv yt-dlp pipewire papirus-icon-theme pipewire-pulse pipewire-alsa libva-intel-driver playerctl dash go zsh noto-fonts-emoji ttf-nerd-fonts-symbols ttf-ibm-plex feh neovim doas yarn npm xsel sway
+sudo pacman -Sy zsh-syntax-highlighting rofi zsh-autosuggestions libvdpau-va-gl xf86-video-amdgpu vulkan-radeon xorg-xwayland libva-mesa-driver qemu virt-manager edk2-ovmf dnsmasq mesa-vdpau mesa pulsemixer qbittorrent bat ripgrep htop ufw ntp wireplumber pipewire-jack foot mpv yt-dlp pipewire papirus-icon-theme pipewire-pulse pipewire-alsa playerctl dash go zsh noto-fonts-emoji ttf-nerd-fonts-symbols ttf-ibm-plex feh neovim doas yarn npm xsel android-tools android-udev libreoffice-fresh-pt-br wine lutris wine-mono winetricks bluez-libs bluez-utils bluez pcmanfm dosfstools vkd3d vulkan-headers mesa lib32-mesa lib32-vulkan-icd-loader lib32-vulkan-radeon
 
 ## Moving folders
 mv $HOME/Arice/config/.zshrc /$HOME/.zshrc && mv $HOME/Arice/config/.config/* /$HOME/.config
@@ -25,10 +25,11 @@ sudo chsh -s /usr/bin/zsh $USER
 touch doas.conf && echo permit nopass "$USER" as root >doas.conf && sudo mv doas.conf /etc/ && sudo chown root:root /etc/doas.conf
 
 ## Installing misc programs in yay
-yay -S pfetch orphan-manager dashbinsh ly mercury-browser-bin
+yay -S pfetch orphan-manager dashbinsh ly ttf ttf-ms-win10-auto
 
 ## Enabling daemons
 sudo systemctl enable ufw.service && sudo systemctl enable ntpd.service && sudo systemctl enable ly.service && sudo systemctl enable libvirtd.socket
+sudo systemctl enable bluetooth.service
 
 ## Setting timezone
 sudo timedatectl set-timezone America/Sao_Paulo
