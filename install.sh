8d0b7902cdff9284693ddba54e8d2d1aa51f77e7
MarcioGCosta <marcio.gustavo.2006@gmail.com>
2025-06-15 21:13:49 -0300

    :-I

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo sed '/Color/s/^#//' -i /etc/pacman.conf && sudo sed '/ParallelDownloads/s/^
 yes | sudo pacman -Sy reflector && sudo reflector --sort score --save /etc/pacman.d/mirrorlist. && sudo rm /etc/pacman.d/mirrorlist && sudo mv /etc/pacman.d/mirrorlist. /etc/pacman.d/mirrorlist
 
 ## Installing misc progams
-sudo pacman -Sy kcalc gvfs zsh-syntax-highlighting zsh-autosuggestions libvdpau-va-gl xf86-video-amdgpu vulkan-radeon libva-mesa-driver qemu virt-manager edk2-ovmf dnsmasq mesa-vdpau mesa pulsemixer qbittorrent htop ufw ntp wireplumber pipewire-jack foot mpv yt-dlp pipewire pipewire-pulse pipewire-alsa playerctl dash go zsh noto-fonts-emoji ttf-nerd-fonts-symbols ttf-ibm-plex feh doas yarn npm xsel android-tools android-udev libreoffice-fresh-pt-br wine lutris wine-mono winetricks bluez-libs bluez-utils bluez pcmanfm-qt dosfstools vkd3d vulkan-headers mesa lib32-mesa lib32-vulkan-icd-loader lib32-vulkan-radeon
+sudo pacman -Sy kcalc gvfs zsh-syntax-highlighting vim zsh-autosuggestions libvdpau-va-gl xf86-video-amdgpu vulkan-radeon libva-mesa-driver qemu virt-manager edk2-ovmf dnsmasq mesa-vdpau mesa pulsemixer qbittorrent htop ufw ntp wireplumber pipewire-jack foot mpv yt-dlp pipewire pipewire-pulse pipewire-alsa playerctl dash go zsh noto-fonts-emoji ttf-nerd-fonts-symbols ttf-ibm-plex feh doas yarn npm xsel android-tools android-udev libreoffice-fresh-pt-br wine lutris wine-mono winetricks bluez-libs bluez-utils bluez pcmanfm-qt dosfstools vkd3d vulkan-headers mesa lib32-mesa lib32-vulkan-icd-loader lib32-vulkan-radeon
 
 ## Moving folders
 mv $HOME/Arice/config/.zshrc /$HOME/.zshrc && mv $HOME/Arice/config/.config/* /$HOME/.config
@@ -21,14 +21,14 @@ git clone https://aur.archlinux.org/yay.git && cd yay* && yes | makepkg -si && c
 ## Changing user shell to zsh
 sudo chsh -s /usr/bin/zsh $USER
 
-## Configuring doas and Configuring neovim for doas/su
+## Configuring doas and 
 touch doas.conf && echo permit nopass "$USER" as root >doas.conf && sudo mv doas.conf /etc/ && sudo chown root:root /etc/doas.conf
 
 ## Installing misc programs in yay
-yay -S pfetch orphan-manager floorp-bin ttf-ms-win10-auto
+yay -S pfetch orphan-manager floorp-bin ttf-ms-win10-auto ly
 
 ## Enabling daemons
-sudo systemctl enable ufw.service && sudo systemctl enable ntpd.service && sudo systemctl enable libvirtd.socket && sudo systemctl enable bluetooth.service
+sudo systemctl enable ufw.service && sudo systemctl enable ntpd.service && sudo systemctl enable libvirtd.socket && sudo systemctl enable bluetooth.service && sudo systemctl enable ly.service
 
 sudo usermod -aG libvirt $USER
 
